962b6f425f717e92b879d769b717381d7ec188e8
sorgazb <sorgazb01@educarex.es>
2025-11-19 20:27:38 +0100

    Add initial project files for data cleaning application including CSV data and ML context setup

---
+++ Tema_02/Tareas/Limpiar DataSet 2/LimpiarDataSet_Clientes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Data;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Transforms;
+
+const string fileInputPath = "datos_clientes.csv";
+const string fileOutputPath = "datos_clientes_preprocessed.csv";
+
+MLContext mlContext = new();
+
+TextLoader.Column[] columns = [
+    new TextLoader.Column("ID_Cliente", DataKind.Single, 0),
+    new TextLoader.Column("Edad", DataKind.Single, 1),
+    new TextLoader.Column("Genero", DataKind.String, 2),
+    new TextLoader.Column("Ingreso_Mensual_USD", DataKind.Single, 3),
+    new TextLoader.Column("", DataKind.Single, 4),
+    new TextLoader.Column("Educación", DataKind.String, 5),
+    new TextLoader.Column("Calificación_Crédito", DataKind.Single, 6),
+    new TextLoader.Column("Tiempo_Empleo", DataKind.Single, 7)
+];
